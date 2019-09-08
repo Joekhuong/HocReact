b2204a1f63129b0d9810d5a32aa237f188f5b44b
Khuong Chu <38976408+Joekhuong@users.noreply.github.com>
2019-09-08 20:52:41 +0300

    Props

--- public/reactFile.js
+++ public/reactFile.js
@@ -1,9 +1,12 @@
+
+{/* create components */}
 class Khuong extends React.Component {
   render() {
     return(
       <div>
         <h1 className="color" >Hello</h1>
-        <OtherComponent/>
+        <OtherComponent name="I am Khuong" job="IT">I am 35 years old</OtherComponent>  {/* create props and special props */}
+        <OtherComponent name="I am Miikka" job="Chef">I am 36 years old</OtherComponent>
       </div>
     );
   }
@@ -13,14 +16,16 @@ class OtherComponent extends React.Component{
   render(){
     return(
       <div>
-        <h3 className="redColor" >I am Chu Nguyen Khuong</h3>
+        <h3 className="redColor" >{this.props.name}, I am working as a {this.props.job}. {this.props.children}</h3>{/* use props and special props */}
       </div>
     );
   }
 }
+
+{/* render components */}
 ReactDOM.render(
   <div>
-    <Khuong/>
+    <Khuong />
   </div>,
   document.getElementById('root')
 );
