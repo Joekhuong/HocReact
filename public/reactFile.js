2e5a2da1bbfe3470cd3548e642c3d7e513dafb56
Khuong Chu <38976408+Joekhuong@users.noreply.github.com>
2019-09-08 18:39:25 +0300

    Split React File

---
+++ public/reactFile.js
@@ -0,0 +1,7 @@
+ReactDOM.render(
+  <h1 className="color">Nguyen Khuong</h1> //className chu khong phia class, vi day ko phai la html, day la ejs
+  /*<div>
+    <h1>Nguyen Khuong</h1>
+    <h1> Chu Nguyen Khuong</h1>
+  </div>*/ //Phai bat buoc co 1 the bao trum lai neu co nhieu the khac trong 1 render.
+  , document.getElementById('root'));
